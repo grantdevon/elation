69fbf54aa977fa7f8faf3edbc4795350846cb9a0
grantdevon <grantsolomons62@gmail.com>
2023-11-15 16:42:51 +0200

    added distance and flatlist

--- app/components/FeedCard/FeedCard.tsx
+++ app/components/FeedCard/FeedCard.tsx
@@ -1,13 +1,17 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React, {FC, useEffect, useState} from 'react';
-import {fetchDocuments} from '../../services/firebase';
 import {IFeedCard} from '../../Interfaces/Feed';
+import { calculateDistance } from '../../Utils/Map'
+import GetLocation from 'react-native-get-location';
 
 const FeedCard: FC<IFeedCard> = ({doc}) => {
   return (
     <View style={styles.container}>
       <View style={styles.cardContainer}>
         <Text>{doc.coords.latitude}</Text>
+        <Text>{doc.coords.longitude}</Text>
+        <Text>Distance: {doc.distance} km's</Text>
+        {/* <Text>{calculateDistance(currentLocation.latitude, currentLocation.longitude, doc.coords.latitude, doc.coords.longitude)} km's from you</Text> */}
       </View>
     </View>
   );
